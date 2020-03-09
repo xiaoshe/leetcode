98d8a7dbb34668cc3e5f4849c456c255376b03c6
shekunlong <shekunlong@xiaomi.com>
2020-03-09 17:29:47 +0800

    add two numbers

--- 2_add_two_numbers.c
+++ 2_add_two_numbers.c
@@ -17,7 +17,7 @@ struct ListNode *make(int v) {
     if (ret == NULL) { \
         ret = p; \
         tail = p; \
-	} else { \
+    } else { \
         tail->next = p; \
         tail = p; \
     }
@@ -34,7 +34,7 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
         int sum = p1->val + p2->val + flag;
         flag = sum / 10;
         struct ListNode *p = make(sum % 10);
-		append(ret, tail, p);
+        append(ret, tail, p);
         p1 = p1->next;
         p2 = p2->next;
     }
@@ -45,13 +45,13 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
         int sum = left->val + flag;
         flag = sum / 10;
         struct ListNode *p = make(sum % 10);
-		append(ret, tail, p);
+        append(ret, tail, p);
         left = left->next;
     }
     // 如果进位
     if (flag > 0) {
         struct ListNode *p = make(flag);
-		append(ret, tail, p);
+        append(ret, tail, p);
     }
     if (ret == NULL) {
         ret = make(0);
@@ -61,22 +61,22 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 }
 
 int main(int argc, char *argv[]) {
-	struct ListNode *h11 = make(2);
-	struct ListNode *h12 = make(4);
-	struct ListNode *h13 = make(3);
-	h11->next = h12;
-	h12->next = h13;
+    struct ListNode *h11 = make(2);
+    struct ListNode *h12 = make(4);
+    struct ListNode *h13 = make(3);
+    h11->next = h12;
+    h12->next = h13;
 
-	struct ListNode *h21 = make(5);
-	struct ListNode *h22 = make(6);
-	struct ListNode *h23 = make(4);
-	h21->next = h22;
-	h22->next = h23;
+    struct ListNode *h21 = make(5);
+    struct ListNode *h22 = make(6);
+    struct ListNode *h23 = make(4);
+    h21->next = h22;
+    h22->next = h23;
 
-	struct ListNode * ret = addTwoNumbers(h11, h21);
-	while (ret != NULL) {
-		printf("%d ", ret->val);
-		ret = ret->next;
-	}
-	printf("\n");
+    struct ListNode * ret = addTwoNumbers(h11, h21);
+    while (ret != NULL) {
+        printf("%d ", ret->val);
+        ret = ret->next;
+    }
+    printf("\n");
 }
